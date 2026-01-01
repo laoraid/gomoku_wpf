1c0df029e4c7a0d874fd59a7a664340257f412b3
Laoraid <a99azaz@gmail.com>
2026-01-01 11:53:59 +0900

    init

---
+++ Gomoku/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Gomoku
+{
+    public enum RequestType
+    {
+        Move, Chat
+    }
+    public abstract class  GameData
+    {
+        public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+    }
+
+    public class PositionData : GameData
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class ChatData : GameData
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+
+    public class ResponseData : GameData
+    {
+        public bool Accepted { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        public RequestType TargetRequest { get; set; }
+    }
+}
---
+++ Gomoku/NetworkSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Gomoku
+{
+    public class NetworkSession
+    {
+        private readonly TcpClient _client;
+        private readonly StreamWriter _writer;
+        private readonly StreamReader _reader;
+
+        public int SessionId { get; }
+
+        public event Action<NetworkSession, GameData>
+    }
+}
