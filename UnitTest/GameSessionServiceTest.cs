7076a84629332325fa8f3c0697301f4958f75fb7
Laoraid <a99azaz@gmail.com>
2026-01-21 23:49:26 +0900

    게임세션서비스 테스트 추가

--- UnitTest/GameSessionServiceTest.cs
+++ UnitTest/GameSessionServiceTest.cs
@@ -27,14 +27,18 @@ namespace UnitTest
             gameSession = new GameSessionService(server, GameClientFactory, messenger);
         }
 
-        [TestMethod]
-        public async Task StartSession_Test()
+        private async Task SetupClient()
         {
             var option = new ConnectionOption("", 1234, "닉네임",
                 DoubleThreeRuleType.WhiteOnlyAllow, ConnectionType.Server, CancellationToken.None);
 
             await gameSession.StartSessionAsync(option);
+        }
 
+        [TestMethod]
+        public async Task StartSession_Test()
+        {
+            await SetupClient();
             await server.Received().StartAsync(1234);
             // 포트 그대로 server에서 생성했는지 확인
             server.Received().AddRule(Arg.Is<DoubleThreeRule>(r => r.DTRuleType == DoubleThreeRuleType.WhiteOnlyAllow));
@@ -46,10 +50,7 @@ namespace UnitTest
         [TestMethod]
         public async Task Game_End_Test()
         {
-            var option = new ConnectionOption("", 1234, "나",
-                DoubleThreeRuleType.WhiteOnlyAllow, ConnectionType.Client, CancellationToken.None);
-            await gameSession.StartSessionAsync(option);
-
+            await SetupClient();
             var me = new Player("나", PlayerType.Observer, new Record(0, 0, 0));
             var player1 = new Player("상대", PlayerType.Observer, new Record(0, 0, 0));
 
@@ -74,6 +75,57 @@ namespace UnitTest
             Assert.IsFalse(gameSession.IsGameStarted);
             Assert.AreEqual(1, me.Records.Win);
             Assert.AreEqual(1, player1.Records.Loss);
+        }
+
+        [TestMethod]
+        public async Task PlaceStone_Should_Change_Turn()
+        {
+            await SetupClient();
+
+            var me = new Player("나", PlayerType.Observer, new Record(0, 0, 0));
+            var player1 = new Player("상대", PlayerType.Observer, new Record(0, 0, 0));
+
+            gameSession.Receive(new ClientJoinResponseData { Me = me, Users = [player1, me] });
+
+            client.Me.Returns(me);
+
+            gameSession.Receive(new GameJoinData { Player = me, Type = PlayerType.Black });
+            gameSession.Receive(new GameJoinData { Player = player1, Type = PlayerType.White });
+
+            gameSession.Receive(new GameStartData());
+
+            var move = new GameMove(5, 5, 1, PlayerType.Black);
+            gameSession.Receive(new PositionData { Move = move });
+
+            Assert.IsTrue(gameSession.IsGameStarted);
+            Assert.AreEqual(PlayerType.White, gameSession.CurrentTurn);
+        }
+
+        [TestMethod]
+        public void JoinPlayer_Disconnect_Should_Be_Null()
+        {
+            var player1 = new Player();
+            player1.Nickname = "흑";
+            var player2 = new Player();
+            player2.Nickname = "백";
+            gameSession.Receive(new ClientJoinData { Player = player1 });
+            gameSession.Receive(new ClientJoinData { Player = player2 });
+
+            gameSession.Receive(new GameJoinData { Player = player1, Type = PlayerType.Black });
+            gameSession.Receive(new GameJoinData { Player = player2, Type = PlayerType.White });
+
+            gameSession.Receive(new GameStartData());
+
+            gameSession.Receive(new ClientExitData { Player = player1 });
+            // 게임 도중 흑 플레이어 나감
+            gameSession.Receive(new GameEndData { EndData = new GameEndMessage(true, PlayerType.White, null, "나감") });
+
+            Assert.AreEqual(1, player2.Records.Win);
+            // 백 플레이어 1승 해야 함
+
+            Assert.IsFalse(gameSession.IsGameStarted);
+            Assert.IsNull(gameSession.BlackPlayer);
+            Assert.IsNotNull(gameSession.WhitePlayer);
         }
     }
 }
