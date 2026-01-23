52b9ee840ebbb129b013f6fa9c5c26c72f2ff9ad
Laoraid <a99azaz@gmail.com>
2026-01-23 20:34:01 +0900

    SQLite 사용 인터페이스 작성

---
+++ Gomoku/Models/DTO/MatchInfo.cs
@@ -0,0 +1,5 @@
+﻿namespace Gomoku.Models.DTO
+{
+    public record MatchInfo(Player BlackPlayer, Player WhitePlayer, PlayerType Winner,
+        IEnumerable<GameMove> MoveHistory, DateTime Time);
+}
---
+++ Gomoku/Services/Interfaces/IDatabaseService.cs
@@ -0,0 +1,16 @@
+﻿using Gomoku.Models;
+using Gomoku.Models.DTO;
+
+namespace Gomoku.Services.Interfaces
+{
+    public interface IDatabaseService
+    {
+        Task<Player?> TryLoginAsync(string id, string hashedpw);
+        Task<Player> CreateAccountAsync(string id, string hashedpw);
+
+        Task SaveMatchAsync(MatchInfo match);
+
+        Task<Record> GetPlayerRecordsAsync(Player player);
+        Task<IEnumerable<MatchInfo>> GetPlayerMachHistoriesAsync(Player player);
+    }
+}
