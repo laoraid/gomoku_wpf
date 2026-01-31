b0c1e0db7d1d8b86c4eff60705476b61d84e02bf
Laoraid <a99azaz@gmail.com>
2026-02-01 00:32:00 +0900

    전적 윈도우 작성중

---
+++ Gomoku/Views/Windows/MatchWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Gomoku.Views.Windows
+{
+    /// <summary>
+    /// MatchWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MatchWindow : Window
+    {
+        public MatchWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
